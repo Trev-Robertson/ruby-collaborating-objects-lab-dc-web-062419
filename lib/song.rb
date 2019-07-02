1c3b9b3d42fd53c117c9afffabb5b2934f4448d7
Trevahr Robertson <trevahr@yahoo.com>
2019-07-02 13:47:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,26 +12,26 @@ class Song
     end
 
     def self.new_by_filename(new_file)
-        # binding.pry
-        # song_info = new_song.gsub(/.mp3/, "").split(" - ")
+    #   if (self.artist.nil?)
+    #   self.artist = Artist.new(name)
+    # else
+    #   self.artist.name = name
+         
          writer, song_name = new_file.split(" - ")
+         
           new_song = self.new(song_name)
-        #   binding.pry
-        #   new_song.artist = writer
-            # binding.pry
-        #   Artist.find_or_create_by_name(song.name)
-        #   binding.pry
-        #   new_song.artist_name = writer
-        # binding.pry
-        # Artist.add_song(song)
-        new_song
-        #Artist:0x00007fe9b8a52068
-            # binding.pry
+          # binding.pry
+          if new_song.artist.nil?
+            new_song.artist = Artist.new(writer)
+            Artist.add_song(new_song)
+          else 
+            new_song.artist_name = writer
+          end
             
     end
 
      def artist_name=(name)
-        binding.pry
+        # binding.pry
        Artist.find_or_create_by_name(name)
     #    binding.pry
      end
